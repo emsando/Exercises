8a0ea4db03200d3928c7bc21e8c89b63e4574f8e
Eric M Sandoval <ericmsandoval@gmail.com>
2018-07-05 17:17:00 -0700

    implements Puzzle class as a container and tracks number of moves

--- towers/towers.js
+++ towers/towers.js
@@ -1,7 +1,14 @@
+class Disk {
+  constructor(size) {
+    this.size = size;
+  }
+}
+
 class Tower {
-  constructor(name) {
+  constructor(name, parent) {
     this.disks = [];
     this.name = name;
+    this.parent = parent;
   }
 
   size() {
@@ -24,100 +31,103 @@ class Tower {
     if (n > 0) {
       this.moveDisks(n - 1, buffer, destination, cb, levels, orderedTowers);
       this.moveTopTo(destination);
-      cb(levels, orderedTowers);
+      this.parent.gameMove++;
+      this.parent.paintPuzzle(levels, orderedTowers);
       buffer.moveDisks(n - 1, destination, this, cb, levels, orderedTowers);
     }
   }
 }
 
-class Disk {
-  constructor(size) {
-    this.size = size;
+class Puzzle {
+  constructor(n) {
+    this.diskNum = n;
+    this.gameMove = 0;
+    this.towers = [
+      new Tower('Tower A', this),
+      new Tower('Tower B', this),
+      new Tower('Tower C', this)
+    ]
+
+    for (let i = n; i > 0; i--) {
+      let disk = new Disk(i)
+      this.towers[0].add(disk);
+    }
   }
-}
 
-const paintGame = (n, towers) => {
-  let string = ''; 
-  let tabs = '\t\t\t';
-  
-  // add tops of towers with spaces
-  for (let i = 0; i < 3; i++) {
-    string += new Array(n).fill(' ').join('');
-    string += '|';
-    string += new Array(n).fill(' ').join('');
-    string += tabs;
-  }
-  string += '\n';
-
-  // towers contain n spaces for disks
-  // for every space
-  for (let i = n - 1; i >= 0; i--) {
-    // for every tower
-    for (let j = 0; j < 3; j++) {
-      let tower = towers[j];
-      let size = tower.size();
-
-      if (size - 1 < i) {
-        // paint spaces
-        string += new Array(n).fill(' ').join('');
-        string += '|';
-        string += new Array(n).fill(' ').join('');
-        string += tabs; 
-      } else {
-        // paint disk
-        let width = n * 2 + 1;
-        let diskSize = tower.disks[i].size;
-        let disk = `=${diskSize}=`;
-        while (diskSize > 1) {
-          disk = '='.concat(disk);
-          disk += '=';
-          diskSize--;
-        }
-        width -= disk.length;
-        width = width / 2;
-        let spaces = '';
-        while (width) {
-          spaces += ' '
-          width--;
-        }
-        string += spaces + disk + spaces;
-        string += tabs;
-      }
+  paintPuzzle() {
+    let string = ''; 
+    let tabs = '\t\t\t';
+    
+    string += `  ** MOVES: ${this.gameMove} **\n`;
+    // add tops of towers with spaces
+    for (let i = 0; i < 3; i++) {
+      string += new Array(this.diskNum).fill(' ').join('');
+      string += '|';
+      string += new Array(this.diskNum).fill(' ').join('');
+      string += tabs;
     }
     string += '\n';
-  }
 
-  // add names to bottom of towers
-  for (let i = 0; i < 3; i++) {
-    let width = n * 2 + 1;
-    let diff = width - 7; // "tower X" is 7 chars
-    let spaces = '';
-    diff = diff / 2;
-    while (diff) {
-      spaces += ' ';
-      diff--;
+    // towers contain n spaces for disks
+    // for every space
+    for (let i = this.diskNum - 1; i >= 0; i--) {
+      // for every tower
+      for (let j = 0; j < 3; j++) {
+        let tower = this.towers[j];
+        let size = tower.size();
+
+        if (size - 1 < i) {
+          // paint spaces
+          string += new Array(this.diskNum).fill(' ').join('');
+          string += '|';
+          string += new Array(this.diskNum).fill(' ').join('');
+          string += tabs; 
+        } else {
+          // paint disk
+          let width = this.diskNum * 2 + 1;
+          let diskSize = tower.disks[i].size;
+          let disk = `=${diskSize}=`;
+          while (diskSize > 1) {
+            disk = '='.concat(disk);
+            disk += '=';
+            diskSize--;
+          }
+          width -= disk.length;
+          width = width / 2;
+          let spaces = '';
+          while (width) {
+            spaces += ' '
+            width--;
+          }
+          string += spaces + disk + spaces;
+          string += tabs;
+        }
+      }
+      string += '\n';
     }
-    string += spaces + towers[i].name + spaces;
-    string += tabs;
+
+    // add names to bottom of towers
+    for (let i = 0; i < 3; i++) {
+      let width = this.diskNum * 2 + 1;
+      let diff = width - 7; // "tower X" is 7 chars
+      let spaces = '';
+      diff = diff / 2;
+      while (diff) {
+        spaces += ' ';
+        diff--;
+      }
+      string += spaces + this.towers[i].name + spaces;
+      string += tabs;
+    }
+    string += '\n';
+
+    console.log(string);
   }
-  string += '\n';
 
-  console.log(string);
-}
-
-const runPuzzle = (n, cb) => {
-  let towerA = new Tower('Tower A');
-  let towerB = new Tower('Tower B');
-  let towerC = new Tower('Tower C');
-  let orderedTowers = [towerA, towerB, towerC]
-
-  for (let i = n; i > 0; i--) {
-    let disk = new Disk(i)
-    towerA.add(disk);
+  runPuzzle() {
+    this.paintPuzzle();
+    this.towers[0].moveDisks(this.diskNum, this.towers[2], this.towers[1], this.towers)
   }
-
-  cb(n, orderedTowers);
-  towerA.moveDisks(n, towerC, towerB, cb, n, orderedTowers);
 }
 
 const driver = () => {
@@ -126,7 +136,9 @@ const driver = () => {
     return;
   }
   let n = Number(process.argv[2]);
-  runPuzzle(n, paintGame);
+  let p = new Puzzle(n);
+
+  p.runPuzzle();
 }
 
 driver();
