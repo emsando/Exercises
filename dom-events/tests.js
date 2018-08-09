607e8a92c249054e398a2a1a6afa90fd60a74340
Eric M Sandoval <ericmsandoval@gmail.com>
2018-08-09 16:01:36 -0700

    dom event testing and further this tests

---
+++ dom-events/tests.js
@@ -0,0 +1,41 @@
+console.log('Test script loaded')
+
+runTests.onclick = () => console.log('This log added via <buttonId>.onclick');
+runTests.addEventListener('click', () => {
+  console.log('This log added via <buttonId>.addEventListener');
+  runTests.onclick = null;
+});
+
+runTests.addEventListener('click', () => {
+  console.log('this with () = {}: ', this);
+})
+
+runTests.addEventListener('click', function() {
+  console.log('this with function() {}: ', this);
+  console.log('================================='); // visual buffer for console logs
+})
+
+// 'parent' causes problems
+parentdiv.addEventListener('click', function(e) {
+  if (e.target !== runTests) return null; // keeps listeners from firing when you click inside the box
+  console.log('parent received click: ', this);
+  console.log('target: ', e.target);
+  console.log('current target: ', e.currentTarget);
+  console.log('================================='); 
+});
+
+grandparent.addEventListener('click', function(e) {
+  if (e.target !== runTests) return null; // keeps listeners from firing when you click inside the box
+  console.log('grandparent received click: ', this);
+  console.log('target: ', e.target);
+  console.log('current target: ', e.currentTarget);
+  console.log('=================================');
+});
+
+// added after parent listeners
+runTests.addEventListener('click', function(e) {
+  console.log('child received click: ', this);
+  console.log('target: ', e.target);
+  console.log('current target: ', e.currentTarget);
+  console.log('=================================');
+})
--- thisTesting.js
+++ thisTesting.js
@@ -12,3 +12,38 @@ let obj = {
 }
 
 obj.func();
+
+const outsideArrowFunc = () => {
+  console.log('Outside () => {} this.foo: ', this.foo);
+}
+
+const outsideFunc = function() {
+  console.log('Outside func(){} this.foo: ', this.foo);
+}
+
+let obj2 = {
+  foo: 'baz',
+  test1: outsideArrowFunc,
+  test2: outsideFunc,
+  test3: () => {
+    console.log('Inside () => {} this.foo: ', this.foo);
+  },
+  test4: function() {console.log('Inside function(){} this.foo: ', this.foo)},
+  test5: function() {
+    this.test3()
+    this.test4.call(this)
+  }
+}
+
+console.log('===============')
+obj2.test1();
+obj2.test2();
+obj2.test3();
+obj2.test4();
+console.log('===============')
+console.log('obj2.test3.call(obj):')
+obj2.test3.call(obj);
+console.log('obj2.test4.call(obj):')
+obj2.test4.call(obj);
+console.log('WRAPPED:')
+obj2.test5();
