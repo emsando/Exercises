7b33594b8925771824261e3b982ff8217a936421
Eric M Sandoval <ericmsandoval@gmail.com>
2018-08-15 14:34:42 -0700

    starter webpack config for exercise apps

---
+++ stockWebpackConfig.js
@@ -0,0 +1,26 @@
+// starter for webpack
+const path = require('path');
+
+const BUILD_DIR = path.join(__dirname, '/client/public');
+const SRC_DIR = path.join(__dirname, '/client/src');
+
+module.exports = {
+  entry: `${SRC_DIR}/App.jsx`,
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: SRC_DIR,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'env'],
+        },
+      },
+    ],
+  },
+};
