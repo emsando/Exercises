51e70269d5a4613f8e219d35b6987f729ee49d06
Eric M Sandoval <ericmsandoval@gmail.com>
2018-09-02 21:10:27 -0700

    adds node modules to git ignore, connect four accepts names from cl

--- cl-connect-four/index.js
+++ cl-connect-four/index.js
@@ -1,6 +1,30 @@
+const inquirer = require('inquirer');
 const { Game } = require('./Game.js');
 
 console.log('Welcome to Connect Four!');
+let playerOne = null;
+let playerTwo = null;
 
-const g = new Game();
-g.runGame();
+const getPlayerNames = () => {
+  return inquirer.prompt({
+    name: 'input_player_one',
+    message: 'Enter name of Player One: ',
+  })
+  .then(({ input_player_one }) => {
+    playerOne = input_player_one
+  })
+  .then(() => {
+    return inquirer.prompt({
+      name: 'input_player_two',
+      message: 'Enter name of Player Two'
+    })
+  })
+  .then(({ input_player_two }) => {
+    playerTwo = input_player_two
+  })
+  .then(() => console.log(playerOne, playerTwo));
+}
+
+getPlayerNames();
+// const g = new Game();
+// g.runGame();
